9052f3bc19fce4f57aaf61ef39d474199a5c020c
yousefmobelal <ym8370302@gmail.com>
2026-03-31 18:21:31 +0200

    implement lucia

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import NavBar from "./sections/NavBar";
 import FirstVideo from "./sections/FirstVideo";
 import Jason from "./sections/Jason";
 import SecondVideo from "./sections/SecondVideo";
+import Lucia from "./sections/Lucia";
 
 gsap.registerPlugin(ScrollTrigger);
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
       <FirstVideo />
       <Jason />
       <SecondVideo />
+      <Lucia />
     </main>
   );
 };
